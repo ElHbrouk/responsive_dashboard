48244163afdb41af2858ab1dc816c34ecfa2f105
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-03-10 18:44:13 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/home_view.dart';
+
+void main() {
+  runApp(const ResponsiveDashBoard());
+}
+
+class ResponsiveDashBoard extends StatelessWidget {
+  const ResponsiveDashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
