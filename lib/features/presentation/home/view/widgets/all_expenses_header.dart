349c99c4077b9bbbf6d07974711d297306a74927
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-23 14:16:47 +0300

    changing the class name to OptionRangeHeader

--- lib/features/presentation/home/view/widgets/all_expenses_header.dart
+++ lib/features/presentation/home/view/widgets/all_expenses_header.dart
@@ -3,21 +3,23 @@ import 'package:flutter/widgets.dart';
 import 'package:responsive_dashboard/core/utils/app_styles.dart';
 import 'package:responsive_dashboard/features/presentation/home/view/widgets/range_options.dart';
 
-class AllExpensesHeader extends StatelessWidget {
-  const AllExpensesHeader({super.key});
-
+class OptionRangeHeader extends StatelessWidget {
+  const OptionRangeHeader({
+    super.key,
+    required this.title,
+  });
+  final String title;
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return  Row(
       children: [
         Text(
-          'All Expenses',
-          style: AppStyles.styleSemiBold20,
+          title,
+          style: AppStyles.styleSemiBold20(context),
         ),
-        Spacer(),
-        RangeOptions(),
+        const Spacer(),
+        const RangeOptions(),
       ],
     );
   }
 }
-
