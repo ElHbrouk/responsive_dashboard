dcaa173589906f2fbac839c3b0830f29cfc5588c
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-23 14:12:49 +0300

    create DetailsItemModel

---
+++ lib/features/data/models/details_item_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailsItemModel {
+  final String title;
+  final String percentage;
+  final Color color;
+
+  const DetailsItemModel({
+    required this.title,
+    required this.percentage,
+    required this.color,
+  });
+}
