41eb10fe649db824f65cc0a76a76f14f49b14786
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-23 15:03:30 +0300

    Refactor MobileLayout to include sections for expenses, quick invoices, cards, transactions, and income
    - Added imports for AllExpensesAndQuickInvoiceSection, MyCardAndTransactionHistorySection, and IncomeSection.
    - Modified the MobileLayout widget to use a SingleChildScrollView containing a Column with the newly imported sections.
    - Updated the constructor of MobileLayout to match the Dart convention for named parameters.
    - Removed the placeholder Text widget in favor of the new layout structure.

--- lib/features/presentation/home/view/widgets/mobile_layout.dart
+++ lib/features/presentation/home/view/widgets/mobile_layout.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/all_expenses_and_quick_invoice_section.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/income_section.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/my_card_and_transaction_history_section.dart';
 
 class MobileLayout extends StatelessWidget {
-  const MobileLayout({super.key});
+  const MobileLayout({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text("MobileLayout"),
+    return const SingleChildScrollView(
+      child:  Column(
+        children: [
+          AllExpensesAndQuickInvoiceSection(),
+          MyCardAndTransactionHistorySection(),
+          IncomeSection(),
+        ],
+      ),
     );
   }
 }
