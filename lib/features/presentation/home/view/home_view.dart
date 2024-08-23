e9f86b0888d5e4ec8cc3f4f0073abe5027eeba3c
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-23 14:14:07 +0300

    adding SizeConfig class

--- lib/features/presentation/home/view/home_view.dart
+++ lib/features/presentation/home/view/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/core/utils/size_config.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/custom_drawer.dart';
 import 'package:responsive_dashboard/features/presentation/home/view/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -6,12 +8,33 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xfff8f9fc),
-      body: SafeArea(
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: width < SizeConfig.tablet
+          ? AppBar(
+              backgroundColor: Colors.grey[100],
+              elevation: 0.0,
+              leading: Builder(builder: (context) {
+                return IconButton(
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                  icon: const Icon(
+                    Icons.menu,
+                  ),
+                );
+              }),
+            )
+          : null,
+      drawer: SizedBox(
+        width: width * 0.6,
+        child: const CustomDrawer(),
+      ),
+      backgroundColor: const Color(0xfff8f9fc),
+      body: const SafeArea(
         child: HomeViewBody(),
       ),
     );
   }
 }
-  
