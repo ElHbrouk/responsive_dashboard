143284593eb5d4a329dfb4dff8a611f0b3ff918c
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-23 14:15:53 +0300

    adding FittedBox

--- lib/features/presentation/home/view/widgets/user_info_list_tile.dart
+++ lib/features/presentation/home/view/widgets/user_info_list_tile.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:responsive_dashboard/core/utils/app_styles.dart';
+import 'package:responsive_dashboard/features/data/models/user_info_model.dart';
 
 class UsreInfoListTile extends StatelessWidget {
-  const UsreInfoListTile(
-      {super.key,
-      required this.image,
-      required this.title,
-      required this.subTitle});
-  final String image, title, subTitle;
+  const UsreInfoListTile({
+    super.key,
+    required this.userInfoModel,
+  });
+  final UserInfoModel userInfoModel;
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 0,
       color: const Color(0xffFAFAFA),
-      child: ListTile(
-        leading: SvgPicture.asset(image),
-        title: Text(title),
-        subtitle: Text(subTitle),
-        titleTextStyle: AppStyles.styleSemiBold16,
-        subtitleTextStyle: AppStyles.styleRegular12,
+      child: Center(
+        child: ListTile(
+          leading: SvgPicture.asset(userInfoModel.image),
+          title: FittedBox(
+            fit: BoxFit.scaleDown,
+            alignment: AlignmentDirectional.centerStart,
+            child: Text(userInfoModel.title),
+          ),
+          subtitle: FittedBox(
+            fit: BoxFit.scaleDown,
+            alignment: AlignmentDirectional.centerStart,
+            child: Text(userInfoModel.subTitle),
+          ),
+          titleTextStyle: AppStyles.styleSemiBold16(context),
+          subtitleTextStyle: AppStyles.styleRegular12(context),
+        ),
       ),
     );
   }
