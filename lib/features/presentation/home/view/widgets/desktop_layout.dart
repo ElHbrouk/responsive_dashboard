ddeeca74460bbe550957ec4269cfc3a0ebc13fbf
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-23 14:56:43 +0300

    adding CustomScrollView

--- lib/features/presentation/home/view/widgets/desktop_layout.dart
+++ lib/features/presentation/home/view/widgets/desktop_layout.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:responsive_dashboard/features/presentation/home/view/widgets/all_expenses.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dashboard/features/presentation/home/view/widgets/custom_drawer.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/income_section.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/my_card_and_transaction_history_section.dart';
 
 class DesktopLayout extends StatelessWidget {
   const DesktopLayout({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const Row(
@@ -14,23 +15,38 @@ class DesktopLayout extends StatelessWidget {
         Expanded(
           child: CustomDrawer(),
         ),
-        SizedBox(
-          width: 32,
-        ),
         Expanded(
           flex: 3,
-          child: Column(
-            children: [
-              Expanded(
-                child: AllExpenses(),
-              ),
-              Expanded(
-                child: SizedBox(),
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: EdgeInsets.only(top: 24.0),
+                        child: AllExpensesAndQuickInvoiceSection(),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(top: 24.0),
+                        child: Column(
+                          children: [
+                            MyCardAndTransactionHistorySection(),
+                            IncomeSection(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
         ),
-        Expanded(flex: 2, child: SizedBox()),
       ],
     );
   }
