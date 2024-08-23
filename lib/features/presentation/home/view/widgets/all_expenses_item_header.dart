438fa45172b93115e0fb90db91310d670e820fc7
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-23 14:17:55 +0300

    adding Flexible and AspectRatio

--- lib/features/presentation/home/view/widgets/all_expenses_item_header.dart
+++ lib/features/presentation/home/view/widgets/all_expenses_item_header.dart
@@ -1,21 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dashboard/features/data/models/all_expenses_item_model.dart';
 
 class AllExpensesItemHeader extends StatelessWidget {
   const AllExpensesItemHeader({
     super.key,
-    required this.image,
+    required this.itemModel,
+    required this.isSelected,
   });
-  final String image;
+  final AllExpensesItemModel itemModel;
+  final bool isSelected;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        CircleAvatar(radius: 30, child: SvgPicture.asset(image)),
+        Flexible(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: 60,
+            ),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor:
+                    isSelected == true ? Colors.blue[300] : Colors.grey[200],
+                child: SvgPicture.asset(
+                  itemModel.image,
+                  colorFilter: ColorFilter.mode(
+                    isSelected == true ? Colors.white : Colors.blue,
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
         const Spacer(),
         Transform.rotate(
           angle: 3.14159265,
-          child: const Icon(Icons.arrow_back_ios),
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: isSelected == true ? Colors.white : Colors.black,
+          ),
         ),
       ],
     );
