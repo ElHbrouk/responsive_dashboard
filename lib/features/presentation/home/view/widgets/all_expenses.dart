d820df153fd8a5d9029bd4620c94a06e1ec00fbe
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-23 14:56:20 +0300

    adding CustomBackgroundWidget

--- lib/features/presentation/home/view/widgets/all_expenses.dart
+++ lib/features/presentation/home/view/widgets/all_expenses.dart
@@ -1,32 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dashboard/features/data/models/all_expenses_item_model.dart';
-import 'package:responsive_dashboard/core/utils/images.dart';
 import 'package:responsive_dashboard/features/presentation/home/view/widgets/all_expenses_header.dart';
-import 'package:responsive_dashboard/features/presentation/home/view/widgets/all_expenses_item.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/all_expenses_item_list_view.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/custom_background_widget.dart';
 
 class AllExpenses extends StatelessWidget {
-  const AllExpenses({super.key});
+  const AllExpenses({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: const Column(
+    return const CustomBackgroundWidget(
+      child: Column(
         children: [
-          AllExpensesHeader(),
-          Expanded(
-            child: AllExpensesItem(
-              itemModel: AllExpensesItemModel(
-                image: Assets.imagesIncome,
-                title: 'Income',
-                date: 'April 2024',
-                price: r'$ 20,129',
-              ),
-            ),
+          OptionRangeHeader(
+            title: 'All Expenses',
           ),
+          SizedBox(
+            height: 16.0,
+          ),
+          AllExpensesItemListView(),
         ],
       ),
     );
