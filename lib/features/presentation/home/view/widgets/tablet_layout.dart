4b75b2c25935f8755e320d7c7f7a5e5dfe4b10dc
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-08-24 17:46:52 +0300

    Refactor TabletLayout to include CustomDrawer and MobileLayout
    The TabletLayout widget has been updated to better utilize space and improve
    user experience on tablet devices. The layout now includes a CustomDrawer for
    navigation and a MobileLayout component for displaying content. This change
    aims to enhance the responsiveness and usability of the application across
    different screen sizes.
    
    The previous implementation of TabletLayout was a simple container with a text
    widget. The new design adopts a row layout, with the CustomDrawer occupying one
    side and the MobileLayout, wrapped in padding for spacing, taking up the
    majority of the screen. This structure allows for a more intuitive and visually
    appealing presentation of content on tablet screens.

--- lib/features/presentation/home/view/widgets/tablet_layout.dart
+++ lib/features/presentation/home/view/widgets/tablet_layout.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/custom_drawer.dart';
+import 'package:responsive_dashboard/features/presentation/home/view/widgets/mobile_layout.dart';
 
 class TabletLayout extends StatelessWidget {
   const TabletLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text("TabletLayout"),
+    return const Row(
+      children: <Widget>[
+        Expanded(child: CustomDrawer()),
+        Expanded(
+          flex: 3,
+          child: Padding(
+            padding:  EdgeInsets.only(top: 24.0),
+            child: MobileLayout(),
+          ),
+        ),
+      ],
     );
   }
 }
